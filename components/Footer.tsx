d138948ba3138310fe99cf39684f708aa8487928
skidev101 <skidev101@gmail.com>
2026-01-30 16:28:07 +0100

    update footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -44,30 +44,25 @@ export const Footer = () => {
               strategy, design, and technology.
             </p>
             <div className="flex gap-4 mt-6 md:mt-4">
-              <Link href="https://facebook.com" target="_blank">
-                <Image
-                  src="/images/social/facebook.svg"
-                  alt="LinkedIn"
-                  width={22}
-                  height={22}
-                />
-              </Link>
-              <Link href="https://instagram.com" target="_blank">
+              <a href="https://www.linkedin.com/company/be-graphic-studio/" target="_blank">
+                <svg width="22px" height="22px" viewBox="0 0 24 24" role="img" xmlns="http://www.w3.org/2000/svg"><title>LinkedIn icon</title><path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433c-1.144 0-2.063-.926-2.063-2.065 0-1.138.92-2.063 2.063-2.063 1.14 0 2.064.925 2.064 2.063 0 1.139-.925 2.065-2.064 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z"/></svg>
+              </a>
+              {/* <a href="https://instagram.com" target="_blank">
                 <Image
                   src="/images/social/instagram.svg"
                   alt="Instagram"
                   width={22}
                   height={22}
                 />
-              </Link>
-              <Link href="https://x.com" target="_blank">
+              </a>
+              <a href="https://x.com" target="_blank">
                 <Image
                   src="/images/social/x.svg"
                   alt="Instagram"
                   width={22}
                   height={22}
                 />
-              </Link>
+              </a> */}
             </div>
           </div>
           <div className="flex flex-col gap-4">
@@ -149,7 +144,7 @@ export const Footer = () => {
         </div>
         <div className="pt-8 border-t border-gray-100 flex flex-col md:flex-row justify-between items-center gap-4 text-center md:text-left">
           <p className="font-heading text-slate-400 text-sm">
-            &copy; {new Date().getFullYear()} BE Graphic Studio. All rights
+            &copy; {new Date().getFullYear()} BE Graphic Studio.j All rights
             reserved.
           </p>
           <div className="flex gap-1">
