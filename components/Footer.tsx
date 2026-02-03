a548980ee6c2e08a54836baaea469cf436cb1a18
Ojomona Ethan Inedu <skidev101@gmail.com>
2026-02-03 22:55:18 +0100

    Update link styling in Footer component

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -149,7 +149,7 @@ export const Footer = () => {
           </p>
           <div className="flex gap-1">
             <p className="text-slate-400 text-sm">Designed and Developed by</p>
-            <a href="https://github.com/skidev101" target="_blank" rel="noopener noreferrer" className="hover:text-blue-accent text-slate-400 text-sm">Monaski</a>
+            <a href="https://github.com/skidev101" target="_blank" rel="noopener noreferrer" className="hover:text-blue-accent text-slate-500 text-sm">Monaski</a>
           </div>
         </div>
       </div>
